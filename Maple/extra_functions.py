7b50637f784c8997ffb4d302bd9cc4a1ebfc4001
Tabi3 <viper0998975@gmail.com>
2022-07-03 18:40:51 +0800

    Fixed some stuff, also destroyed main.py

--- Maple/extra_functions.py
+++ Maple/extra_functions.py
@@ -1,6 +1,6 @@
-import json, nextcord
+import asyncio, json, nextcord, os
 from nextcord.ext import commands
-import os
+os.chdir("./Maple")
 
 def get_prefix(client, message):
     with open('./json/prefixes.json', 'r') as f:
@@ -8,3 +8,12 @@ def get_prefix(client, message):
             return json.load(f)[str(message.guild.id)]
         except Exception:
             return "."
+
+class myClient(commands.Bot):
+    def __init__(self, cp=get_prefix, description = None, **options):
+        super().__init__(cp, description = description, **options)
+        [self.load_extension(f"cogs.{j}.{i[:-3]}") for j in os.listdir("./cogs") for i in os.listdir(f"./cogs/{j}") if i.endswith(".py")]
+        self.run("token")
+        return self
+
+
--- Maple/main.py
+++ Maple/main.py
@@ -1,5 +1,2 @@
-import nextcord
-from nextcord.ext import commands
-import extra_functions, os
-os.chdir("./Maple")
-commands.Bot(command_prefix=extra_functions.get_prefix, intents=nextcord.Intents.all()).load_extensions([f"cogs.{j}.{i[:-3]}" for j in os.listdir("./cogs") for i in os.listdir(f"./cogs/{j}") if i.endswith(".py")]).run("ODQxMzYyMDg2NjY2OTYwOTI2.YJlpgQ.4w3mHvPHOYxwMKJwCDQh3eg2WQ8") if __name__ == '__main__' else 0
+import nextcord, extra_functions
+extra_functions.myClient(intents=nextcord.Intents.all()) if __name__ == '__main__' else 0

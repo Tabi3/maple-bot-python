e252345656c997568693cc60784450d74316ece6
Tabi3 <viper0998975@gmail.com>
2022-07-18 11:21:19 +0800

    a

--- Maple/test.py
+++ Maple/test.py
@@ -1,7 +1,12 @@
-import requests, asyncio
+import asyncio
+
+async def derivative(f):
+    return lambda x: (f(x+1e-2)-f(x-1e-2))/2e-2
 
 async def main():
-    print(requests.post('http://localhost:5000/flask/start_maple').json()['msg'])
+    f = lambda x: x**2
+    print((await derivative(f))(1))
+    ...
 
 if __name__ == "__main__":
     asyncio.run(main())
